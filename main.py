4ccaafc58149916d24a55772018925e721cc2bf5
Ziyodullo Elmurodov <Ziyodev>
2025-01-25 17:48:08 +0500

    add count

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters
 from telegram import Update,InlineKeyboardButton,InlineKeyboardMarkup
 import os 
-
+ldl_count = {'like':0,'dislike':0}
 
 def start(update: Update, context):
     update.message.reply_text("Salom. Menga rasm yuboring!")
@@ -17,7 +17,13 @@ def ilb(update: Update, context: CallbackContext):
     inline_button = InlineKeyboardButton(text="Codewars", url="https://www.codewars.com")    
     inline_keyboard = InlineKeyboardMarkup([[inline_button]])
     update.message.reply_text("Codewarsga o'tish uchun tugmani bosing", reply_markup=inline_keyboard)
-
+def count(update: Update, context: CallbackContext):
+    if update.message.text=='like':
+        ldl_count['like']+=1
+    else:
+        ldl_count['dislike']+=1
+        
+    update.message.reply_text(f"Like: {ldl_count['like']} Dislike: {ldl_count['dislike']}")
 TOKEN = os.getenv("TOKEN")
 
 updater = Updater(token=TOKEN)
@@ -25,6 +31,7 @@ updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(MessageHandler(Filters.photo,replyphoto))
 dispatcher.add_handler(CommandHandler('masala',ilb))
+dispatcher.add_handler(CommandHandler('ovozlar',count))
 dispatcher.add_handler(CommandHandler('start',start))
 
 updater.start_polling()
