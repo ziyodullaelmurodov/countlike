b4612455b970f28b6403914b020dc3b79bf3c2a1
Ziyodullo Elmurodov <Ziyodev>
2025-01-25 16:01:44 +0500

    add replyphoto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater,CommandHandler
+from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters
 from telegram import Update
 import os 
 
@@ -6,12 +6,16 @@ import os
 def start(update: Update, context):
     update.message.reply_text("Salom. Menga rasm yuboring!")
 
+def replyphoto(update: Update, context: CallbackContext):
+    ph=update.message.photo[-1].file_id
+    update.message.reply_photo(photo=ph)
+
 TOKEN = os.getenv("TOKEN")
 
 updater = Updater(token=TOKEN)
 
 dispatcher = updater.dispatcher
-
+dispatcher.add_handler(MessageHandler(Filters.photo,replyphoto))
 dispatcher.add_handler(CommandHandler('start',start))
 
 updater.start_polling()
